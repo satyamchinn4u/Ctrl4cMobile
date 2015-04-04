6c3ce98abfb8f76998e695abb65afc792bc497c9
satyamchinn4u <satyamchinna4u@gmail.com>
2015-04-04 11:52:05 +0530

    session
    session test

---
+++ js/sessioncheck.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+
+if(typeof(localStorage.getItem("ctrl4c"))!='undefined' || localStorage.getItem("ctrl4c") != null ||(localStorage["ctrl4c"]))
+{
+	window.location="dashboard.html";
+	
+}
